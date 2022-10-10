998dbaf1126acfc2c324f47450590a61b0c5f07a
Brandon <brandon101morgan@outlook.com>
2022-10-10 16:22:01 -0500

    Add files via upload

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
 from users.models import Post
-
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 admin.site.register(Post)
+
+
+
+
+
--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,11 @@
 from dataclasses import fields
+import imp
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
-
+from .models import User
+from users import models
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
@@ -13,7 +15,6 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
-
 
  
  
